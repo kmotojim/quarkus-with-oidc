154845ef4afeb93e1150d0e9a141582865756614
kmotojim <kmotojim@redhat.com>
2023-06-03 23:25:54 +0900

    initial

--- src/main/java/org/acme/security/openid/connect/template/WebResource.java
+++ src/main/java/org/acme/security/openid/connect/template/WebResource.java
@@ -5,9 +5,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-import org.eclipse.microprofile.jwt.JsonWebToken;
-
-import io.quarkus.oidc.IdToken;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import lombok.extern.slf4j.Slf4j;
@@ -17,20 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 public class WebResource {
 
     @Inject
-    @IdToken
-    JsonWebToken idToken;
-
-    @Inject
     Template hello;
 
     @Inject
     Template privatePage;
-
-    @Inject
-    Template protectedPage;
-
-    @Inject
-    Template notProtectedPage;
 
 
     @GET
@@ -57,27 +44,4 @@ public class WebResource {
                 ;   
     }
 
-    @GET
-    @Path("/protected-page")
-    public TemplateInstance getProtectedPage() {
-
-        log.info("This is protected-page");
-        log.info("idToken: {}",idToken);
-        
-        return protectedPage
-                .data("pageType", "Protected")
-                .data("idToken", idToken)
-                ;  
-    }
-
-    @GET
-    @Path("/not-protected-page")
-    public TemplateInstance getNotProtectedPage() {
-
-        log.info("This is not-protected-page");
-        
-        return notProtectedPage
-                .data("pageType", "NotProtected")
-                ;  
-    }
 }
